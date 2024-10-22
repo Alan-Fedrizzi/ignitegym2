b9d132a8811665142d245f5b7c3b755466e975bf
alanfedrizzi <alanfedrizzi@gmail.com>
2024-10-22 11:39:02 +0200

    novo app com gluestack

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { GluestackUIProvider, Text, Center } from "@gluestack-ui/themed";
+// esse é o tema padrão
+// import { config } from "@gluestack-ui/config";
+// esse é o tema que alteramos
+import { config } from "./config/gluestack-ui.config";
+
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  // useFonts retorna um array, na primeira posição um boolean
+  const [fonstLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // GluestackUIProvider fornece os componentes para a aplicação
+    // envolvemos tudo nele
+    // config é para usar o tema padrão
+    <GluestackUIProvider config={config}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fonstLoaded ? (
+        <Center flex={1} bg="$gray700">
+          {/* <Text color="white" fontSize={34}> */}
+          <Text>Home</Text>
+        </Center>
+      ) : (
+        <Loading />
+      )}
+    </GluestackUIProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// 2 Component Library
+// Imagem de background
