7d8b3763ba8ea969fc6f14359d921f2e4cc8cd74
alanfedrizzi <alanfedrizzi@gmail.com>
2025-01-09 11:00:24 +0100

    galeria de fotos..

--- App.tsx
+++ App.tsx
@@ -33,3 +33,4 @@ export default function App() {
 }
 
 // 6 galeria de fotos
+// utilizando mensagens toast...
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,25 +1,82 @@
-import { ScrollView, TouchableOpacity } from "react-native";
+import { Alert, ScrollView, TouchableOpacity } from "react-native";
 import { Center, VStack, Text, Heading } from "@gluestack-ui/themed";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { useState } from "react";
+import { ToastMessage } from "@components/ToastMessage";
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState(
+    "https://github.com/Alan-Fedrizzi.png"
+  );
+
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+        // base64: true,
+      });
+
+      if (photoSelected.canceled) return;
+
+      // in bytes, mas é optional, pode não vir..
+      // console.log(photoSelected.assets[0].fileSize);
+      // vamos usar o Expo FileSystem
+
+      // console.log(photoSelected.assets[0]);
+      const photoUri = photoSelected.assets[0].uri;
+
+      if (!photoUri) return;
+
+      const photoInfo = (await FileSystem.getInfoAsync(photoUri)) as {
+        size: number;
+        // para dizer que dentro vai ter um size que é number
+      };
+      console.log(photoInfo.size);
+
+      const isBiggerThanFiveMb = photoInfo.size / 1024 / 1024 > 5;
+
+      if (photoInfo.size && isBiggerThanFiveMb) {
+        return Alert.alert(
+          "Essa imagem é muito grande. Escolha uma de até 5MB."
+        );
+      }
+
+      setUserPhoto(photoUri);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
 
+      <ToastMessage
+        id="1"
+        title="titulo"
+        description="descricao"
+        action="success"
+        onClose={() => {}}
+      />
+
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
-            source={{ uri: "https://github.com/Alan-Fedrizzi.png" }}
+            source={{ uri: userPhoto }}
             size="xl"
             alt="Foto do usuário"
           />
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="$green500"
               fontFamily="$heading"
